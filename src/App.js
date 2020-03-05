b5774bc85916fbaac89123ae0e2f699328c7ca8e
JamesYang <gn00999829@gmail.com>
2020-03-05 02:57:37 -0800

    putting Login and Singup in one component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import React , { useEffect, useContext }from "react";
-import { BrowserRouter, Switch } from 'react-router-dom';
-import "./App.css";
-import {LoginModal} from './components/LoginModal/LoginModal.jsx';
-import {SigninModal} from './components/SigninModal/SigninModal';
+import React, {useEffect, useContext} from 'react';
+import {BrowserRouter, Switch} from 'react-router-dom';
+import './App.css';
+import {LoginPage} from './components/LoginPage/LoginPage';
 import Timeline from './components/Timeline';
-import Profile from './components/Profile'
+import Profile from './components/Profile';
 
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import firebase from './plugins/firebase';
-import { AuthContext } from './contexts/AuthContext';
+import {AuthContext} from './contexts/AuthContext';
 
 function App() {
-  const { login } = useContext(AuthContext);
-  useEffect(()=>{
+  const {login} = useContext(AuthContext);
+  useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
-      if(user) {
-        login(user)
+      if (user) {
+        login(user);
       }
-    })
-  },[])
+    });
+  }, []);
   return (
     <BrowserRouter>
-    <Switch>
-      {/* <PublicRoute restricted={true} component={LoginModal} path="/" exact /> */}
-      <PublicRoute restricted={true} component={SigninModal} path="/" exact />
-      <PrivateRoute component={Timeline} path="/timeline" exact />
-      <PrivateRoute component={Profile} path="/profile" exact />
-    </Switch>
-  </BrowserRouter>
+      <Switch>
+        <PublicRoute restricted={true} component={LoginPage} path="/" exact />
+        <PrivateRoute component={Timeline} path="/timeline" exact />
+        <PrivateRoute component={Profile} path="/profile" exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
